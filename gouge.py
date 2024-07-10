df559826a0582996133c051b6d6d199753682ec3
mqst <muqsitbaig@gmail.com>
2024-07-10 13:15:50 +0530

    Wrote IScannerCheck class + 3 methods from it

--- gouge.py
+++ gouge.py
@@ -1,1 +1,18 @@
-import burp from IBurpExtender
+from burp import IBurpExtender, IScannerCheck, IScanIssue
+
+class BurpExtender(IBurpExtender, IScannerCheck):   
+    def registerExtenderCallbacks(self, callbacks):
+        self._callbacks = callbacks
+        self._helpers = callbacks.getHelpers()
+        callbacks.setExtensionName("Gouge")
+        print("Gouge extension loaded")
+
+    def doPassiveScan(self, baseRequestResponse):
+        pass
+    def doActiveScan(self, baseRequestResponse, insertionPoint):
+        pass
+    def consolidateDuplicateIssues(self, existingIssue, newIssue):
+        if existingIssue.getIssueName() == newIssue.getIssueName():
+            return -1
+        return 0
+
